3f43478c51db345d93efaffc1c7b0039fdd0d18a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-03-16 19:45:32 +0530

    Arduino: WPF interaction

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -9,6 +9,9 @@ MPU6050 mpu;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Connecting to WPF");
+  validatePortConnection();
+  Serial.println("Connected to WPF");
   Serial.println("Initialize MPU6050");
   while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
     Serial.println("MPU6050 sensor not detected");
@@ -28,10 +31,53 @@ void setup() {
 }
 
 void loop() {
-  String serialOut = getPitchRoll(); //Get Pitch and Roll values
+  wpfSerialRead();
+  String serialOut="Reading>> ";
+  serialOut += getPitchRoll(); //Get Pitch and Roll values
   serialOut += getVibration(); //Get Vibration value
   serialOut += getLocation();//Get location coordinates
   Serial.println(serialOut); // Final Serial Like: pitch roll vibration latitude longitude altitude (1 0 3.8432 27.2046 77.4977) Split values from single space " "
+}
+
+void validatePortConnection(){
+  Serial.print("Response>> ");
+  while(true){
+    char serialIn=Serial.read();
+    if(serialIn=='C'){ //Sent by WPF
+      break;
+    }
+  }
+}
+
+void wpfSerialRead(){
+  Serial.print("Response>> ");
+  while(true){
+    char serialIn=Serial.read();
+    if(serialIn=='c'){  //Continue Reading
+      Serial.println("Continue Reading");
+      break;
+    }
+    if(serialIn=='p'){  //Pause the device
+      Serial.println("Pause Reading");
+      pauseDevice();
+      break;
+    }
+    if(serialIn=='e'){  //Call Emergency
+      Serial.println("Call Emergency");
+      callEmergency();
+    }
+  }
+}
+
+void pauseDevice(){
+  Serial.print("Response>> ");
+  while(true){
+    char serialIn=Serial.read();
+    if(serialIn=='c'){
+      Serial.println("Continue Reading");
+      break;
+    }
+  }
 }
 
 String getPitchRoll() {
@@ -54,4 +100,9 @@ String getLocation() {
 
 void callEmergency() {
   // GSM: calling emergency gose here
+  afterCallAction();
+}
+
+void afterCallAction(){
+  // Instructions after calling emergency
 }
