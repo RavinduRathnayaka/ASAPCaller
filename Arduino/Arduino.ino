085ad12f99e6c7c708aab5a172e8c35263670fcc
Haritha Rathnayake <chamararathnayaka@outlook.com>
2022-05-11 17:02:15 +0530

    Ui: Edited Settings Button

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,3 +1,22 @@
+#include <ArduinoWiFiServer.h>
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
 //For Arduino Uno
 //A5 SCL
 //A4 SDA
